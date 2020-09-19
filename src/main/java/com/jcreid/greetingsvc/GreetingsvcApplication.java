08e68cc6264ede4be371aa159f3a39f4ca20692d
reidji <jim.reid@ihg.com>
2020-09-19 08:04:32 -0400

    Initial creation

---
+++ src/main/java/com/jcreid/greetingsvc/GreetingsvcApplication.java
@@ -0,0 +1,13 @@
+package com.jcreid.greetingsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingsvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcreid/greetingsvc/GreetingsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcreid.greetingsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
