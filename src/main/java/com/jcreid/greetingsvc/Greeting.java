8a8d772227807b9dbb11fd2eb8208c47e9f611b3
reidji <jim.reid@ihg.com>
2020-09-20 08:35:50 -0400

    added dummy id to trigger build

--- src/main/java/com/jcreid/greetingsvc/Greeting.java
+++ src/main/java/com/jcreid/greetingsvc/Greeting.java
@@ -4,10 +4,12 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    private final long dummyID;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+        this.dummyID = 1;
     }
 
     public long getId() {
