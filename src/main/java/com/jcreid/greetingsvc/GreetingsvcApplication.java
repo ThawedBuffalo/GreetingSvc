f9d9e7eec13e93586b6a9ce462916778209856de
reidji <jim.reid@ihg.com>
2020-09-21 13:01:12 -0400

    added dockerfile and changed default port

--- src/main/java/com/jcreid/greetingsvc/GreetingsvcApplication.java
+++ src/main/java/com/jcreid/greetingsvc/GreetingsvcApplication.java
@@ -3,11 +3,16 @@ package com.jcreid.greetingsvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class GreetingsvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GreetingsvcApplication.class, args);
+        SpringApplication app = new SpringApplication(GreetingsvcApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
     }
 
 }
